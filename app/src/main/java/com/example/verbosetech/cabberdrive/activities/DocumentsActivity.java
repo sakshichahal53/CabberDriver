990e973bc2b232bd2fc68585746814cbdc1abea8
Sakshi Chahal <sakshichahal53@gmail.com>
2018-07-02 11:52:44 +0530

    two layouts remain

--- app/src/main/java/com/example/verbosetech/cabberdrive/activities/DocumentsActivity.java
+++ app/src/main/java/com/example/verbosetech/cabberdrive/activities/DocumentsActivity.java
@@ -2,6 +2,7 @@ package com.example.verbosetech.cabberdrive.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.verbosetech.cabberdrive.R;
 
@@ -10,6 +11,14 @@ public class DocumentsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_documents);
+        setContentView(R.layout.home_offline);
+
+
+        Toolbar toolbar=findViewById(R.id.toolbar_home_offline);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
     }
 }
